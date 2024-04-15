d4a2c1ad0cf3cb6b61ef22c31b2fb838e49653d3
Victor E González P <vemiliogp@gmail.com>
2024-04-15 11:43:37 +0200

    First commit

---
+++ src/presentation/server.ts
@@ -0,0 +1,37 @@
+import express from "express";
+import path from "path";
+
+interface Options {
+  port: number;
+  publicPath?: string;
+}
+
+export class Server {
+  private app = express();
+  private readonly port: number;
+  private readonly publicPath: string;
+
+  constructor(options: Options) {
+    const { port, publicPath = "public" } = options;
+
+    this.port = port;
+    this.publicPath = publicPath;
+  }
+
+  async start() {
+    // Middlewares
+
+    // Public folder
+    this.app.use(express.static(this.publicPath));
+
+    this.app.get("*", (req, res) => {
+      const indexPath = path.join(__dirname, `../../${this.publicPath}/index.html` );
+      res.sendFile(indexPath);
+      return;
+    });
+
+    this.app.listen(this.port, () => {
+      console.log(`Server running on port ${this.port}`);
+    });
+  }
+}
